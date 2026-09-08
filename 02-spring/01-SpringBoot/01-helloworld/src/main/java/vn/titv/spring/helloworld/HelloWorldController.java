68c5f140da70a8f92b52c9d1b6a4402f28f64216
huukhann <huukhangdann@gmail.com>
2026-09-08 16:00:53 +0700

    Inject Configuration Properties

--- 02-spring/01-SpringBoot/01-helloworld/src/main/java/vn/titv/spring/helloworld/HelloWorldController.java
+++ 02-spring/01-SpringBoot/01-helloworld/src/main/java/vn/titv/spring/helloworld/HelloWorldController.java
@@ -1,12 +1,30 @@
 package vn.titv.spring.helloworld;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
+
+    @Value("${khoahoc.name}")
+    String KhoaHocName;
+
+    @Value("${khoahoc.duration}")
+    String KhoaHocDuration;
+
+    @Value("${khoahoc.notes}")
+    String KhoaHocNotes;
+
     @GetMapping("/")
     public String index(){
         return "Hello World!";
+    }
+
+    @GetMapping("/khoahoc")
+    public String index2(){
+        return "Hello, chào mừng bạn đến với khóa học " + KhoaHocName + "<br/>"
+                + "Thời lượng của khóa học này là " + KhoaHocDuration + "<br/>"
+                + KhoaHocNotes;
     }
 }
