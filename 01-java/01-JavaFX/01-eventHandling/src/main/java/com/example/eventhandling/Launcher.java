a4bf27f799e8196fca62091869360d9e2be22412
huukhann <huukhangdann@gmail.com>
2026-09-17 13:52:20 +0700

    upload PJ

---
+++ 01-java/01-JavaFX/01-eventHandling/src/main/java/com/example/eventhandling/Launcher.java
@@ -0,0 +1,9 @@
+package com.example.eventhandling;
+
+import javafx.application.Application;
+
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(HelloApplication.class, args);
+    }
+}
