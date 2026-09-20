f4d75753b86899d72a4338a6c8829109b9bf8620
huukhann <huukhangdann@gmail.com>
2026-09-21 03:53:45 +0700

    tasks 11 - Adding weights

--- 01-java/01-JavaFX/03-MiniProjectReviewCourse/src/main/java/com/example/_3miniprojectreviewcourse/HelloController.java
+++ 01-java/01-JavaFX/03-MiniProjectReviewCourse/src/main/java/com/example/_3miniprojectreviewcourse/HelloController.java
@@ -1,11 +1,15 @@
 package com.example._3miniprojectreviewcourse;
 
+import javafx.beans.binding.Bindings;
+import javafx.beans.binding.DoubleBinding;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Line;
+import javafx.scene.text.Text;
 
 public class HelloController {
     @FXML
@@ -23,7 +27,7 @@ public class HelloController {
         myPane.setOnMouseClicked(eventPane -> {
             double x = eventPane.getX();
             double y = eventPane.getY();
-            Circle circle = new Circle(x, y, 15);
+            Circle circle = new Circle(x, y, 10);
             System.out.println(count + " circle created!");
             count++;
 
@@ -61,18 +65,42 @@ public class HelloController {
     }
 
     private void makeLine(Circle circle1, Circle circle2) {
+        // make line and binding it to the vertex
         Line line = new Line();
         line.startXProperty().bind(circle1.centerXProperty());
         line.startYProperty().bind(circle1.centerYProperty());
-
         line.endXProperty().bind(circle2.centerXProperty());
         line.endYProperty().bind(circle2.centerYProperty());
+
+        // add weight for the edge
+        TextField textField = new TextField();
+        double midx = (circle1.getCenterX() + circle2.getCenterX()) / 2;
+        double midy = (circle1.getCenterY() + circle2.getCenterY()) / 2;
+        textField.setLayoutX(midx);
+        textField.setLayoutY(midy);
+        textField.setMinWidth(3);
+        textField.setOnAction(event -> {
+            String value = textField.getText();
+            int weight = Integer.parseInt(value);
+            System.out.println(weight);
+            Text weightText = new Text(value);
+            weightText.setFill(Color.WHITE);
+            weightText.setId("WeightText");
+            weightText.setMouseTransparent(true);
+            weightText.layoutXProperty().bind(Bindings.divide(Bindings.add(circle1.centerXProperty(), circle2.centerXProperty()), 2));
+            weightText.layoutYProperty().bind(Bindings.divide(Bindings.add(circle1.centerYProperty(), circle2.centerYProperty()), 1.85));
+            myPane.getChildren().remove(textField);
+            myPane.getChildren().add(weightText);
+        });
+
         line.setStrokeWidth(2);
         line.setMouseTransparent(true); // Disable mouse for lines
         myPane.getChildren().add(line);
+        myPane.getChildren().add(textField);
         firstVertex = null;
         circle1.setFill(Color.BLACK);
         circle2.setFill(Color.BLACK);
     }
+
 }
 
